ebe0751204d862704209b45dbb787fbe11a5e4ec
Peter St. John <peterc.stjohn@gmail.com>
2017-01-10 10:08:46 -0700

    first commit

---
+++ d3flux/test/create_test_html.py
@@ -0,0 +1,14 @@
+from cobra.io import load_json_model
+from d3flux import flux_map
+
+new_model = load_json_model('simple_model.json')
+
+new_model.objective = new_model.reactions.R4
+new_model.reactions.R8.knock_out()
+new_model.optimize()
+
+html = flux_map(new_model, figsize=(300,250))
+with open('test.html', 'w') as f:
+    f.write('<!DOCTYPE html> <html> <head> <title>Test d3flux page</title> <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.2/require.min.js" integrity="sha256-Vjusm6Kh2U7/tb6jBh+MOfxnaf2TWsTph34bMKhC1Qc=" crossorigin="anonymous"></script> <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> </head> <body>')
+    f.write(html.data)
+    f.write('</body> </html>')
